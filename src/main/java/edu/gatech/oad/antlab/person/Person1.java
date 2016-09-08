1f62e24238227c768564a686598c3022ce442279
Matthew Lemons <Matthew Lemons>
2016-09-08 18:59:49 -0400

    Fix calc method and update information in Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String front = input.substring(2);
+	  String end = input.substring(0,2);
+	  return front.concat(end);
 	}
 	
 	/**
