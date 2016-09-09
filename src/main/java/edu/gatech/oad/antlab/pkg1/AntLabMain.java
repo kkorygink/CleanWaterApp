7e151df1bb4ca6ccb4085518e722d28c36df6631
Dilip Shekhar <dshekhar3@gatech.edu>
2016-09-09 16:27:13 -0400

    editing AntLabMain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -81,8 +81,8 @@ public class AntLabMain {
 
         //Person5 replace P4 with your name
         //and gburdell5 with your gt id
-        Person5 p5 = new Person5("P5");
-        toPrint += p5.toString("gburdell5");
+        Person5 p5 = new Person5("Dilip Shekhar");
+        toPrint += p5.toString("dshehkar3");
 
         System.out.println(toPrint);
 
