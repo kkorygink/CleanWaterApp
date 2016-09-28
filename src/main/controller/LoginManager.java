abda91ea48fc3e57114aba3a496f3d81b9d95eb3
Zara Farooq <z.sairan@gmail.com>
2016-09-28 00:44:59 -0400

    Added registration window and modified other windows.

--- src/main/controller/LoginManager.java
+++ src/main/controller/LoginManager.java
@@ -33,7 +33,7 @@ public class LoginManager {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/welcome.fxml"));
             primaryStage.setTitle("Water App - Welcome");
-            primaryStage.setScene(new Scene(loader.load(), 290, 80));
+            primaryStage.setScene(new Scene(loader.load(), 500, 400));
             primaryStage.show();
 
             WelcomeController controller = loader.getController();
@@ -47,7 +47,7 @@ public class LoginManager {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/login.fxml"));
             primaryStage.setTitle("Water App - Login");
-            primaryStage.setScene(new Scene(loader.load(), 460, 190));
+            primaryStage.setScene(new Scene(loader.load(), 600, 300));
             primaryStage.show();
 
             LoginController controller = loader.getController();
@@ -59,10 +59,11 @@ public class LoginManager {
 
     public void showRegister() {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("../view/register.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../view/registration.fxml"));
             primaryStage.setTitle("Water App - Register");
-            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.setScene(new Scene(root, 600, 500));
             primaryStage.show();
+
         } catch (IOException ex) {
             Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -72,7 +73,7 @@ public class LoginManager {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/main.fxml"));
             primaryStage.setTitle("Water App");
-            primaryStage.setScene(new Scene(loader.load(), 300, 275));
+            primaryStage.setScene(new Scene(loader.load(), 500, 500));
             primaryStage.show();
 
             MainController controller = loader.getController();
--- src/main/controller/RegisterController.java
+++ src/main/controller/RegisterController.java
@@ -1,7 +1,45 @@
 package main.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import main.model.User;
+import main.model.UserType;
+
 /**
  * Created by random on 9/20/16.
  */
 public class RegisterController {
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField email;;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private ComboBox<UserType> accountType;
+    @FXML
+    private PasswordField confirmPassword;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Button cancelButton1;
+
+    @FXML
+    public void initialize() {
+        accountType.getItems().setAll(UserType.values());
+    }
+
+    public void initManager(final LoginManager loginManager) {
+        cancelButton1.setOnAction((ActionEvent event) -> {
+            loginManager.showWelcome();
+        });
+
+        registerButton.setOnAction((ActionEvent event) -> {
+            loginManager.showLogin();
+        });
+    }
+
 }
